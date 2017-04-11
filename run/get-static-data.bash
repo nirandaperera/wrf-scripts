6a2f8276f3b6ec9a2cd56b6f321e2833ca838029
uwcc-admin <uwcc-admin@wrf-381-base-v1.c.uwcc-160712.internal>
2017-04-11 07:27:51 +0000

    minor change

--- run/get-static-data.bash
+++ run/get-static-data.bash
@@ -5,9 +5,9 @@ rundate=$(date '+%Y%m%d'  --date="1 days ago")
 #rundate1="2016-08-09"
 echo "Downoading static geog data"
 
-gfs_home="/mnt/disks/wrf-mod/DATA/geog/"
-mkdir -p $gfs_home
-cd $gfs_home || exit
+data_home="/mnt/disks/wrf-mod/DATA/"
+mkdir -p $data_home
+cd $data_home || exit
 
 geog_dataset="geog_complete.tar.bz2"
 geog_location="http://www2.mmm.ucar.edu/wrf/src/wps_files/$geog_dataset"
@@ -15,3 +15,7 @@ geog_location="http://www2.mmm.ucar.edu/wrf/src/wps_files/$geog_dataset"
 wget "$geog_location"
 
 tar jxf $geog_dataset
+
+rm -f $geog_dataset
+
+echo "Downoading static geog data: Completed!"
