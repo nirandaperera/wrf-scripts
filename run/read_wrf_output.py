c5a01a32c8778082cf8c6e26c9f511205935d5fe
nirandaperera <niranda.perera@gmail.com>
2017-04-20 16:56:05 +0530

    creting the kelani basin rainfall file

--- run/read_wrf_output.py
+++ run/read_wrf_output.py
@@ -9,13 +9,6 @@ import csv
 
 from netCDF4 import Dataset  # http://code.google.com/p/netcdf4-python/
 
-# GLOBALS
-wrf_home = '/home/nira/Desktop/temp'
-wrf_output = '/home/nira/Desktop/temp'
-weather_stations = '/home/nira/IdeaProjects/WrfRainfall/stations.txt'
-start_date = dt.datetime.strptime('2017-03-27', '%Y-%m-%d')
-end_date = dt.datetime.strptime('2017-03-28', '%Y-%m-%d')
-
 
 def extract_time_data(_nc_fid):
     _times_len = len(_nc_fid.dimensions['Time'])
@@ -60,7 +53,7 @@ def ncdump(_nc_fid, verb=True):
     return _nc_attrs, _nc_dims, _nc_vars
 
 
-def extract_metro_colombo(_nc_fid, _date, _times):
+def extract_metro_colombo(_nc_fid, _date, _times, _wrf_output):
     lat_min = 41
     lat_max = 47
     lon_min = 11
@@ -81,7 +74,7 @@ def extract_metro_colombo(_nc_fid, _date, _times):
     width = len(lons)
     height = len(lats)
 
-    output_dir = wrf_output + '/colombo/created-' + _date.strftime('%Y-%m-%d')
+    output_dir = _wrf_output + '/colombo/created-' + _date.strftime('%Y-%m-%d')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -105,10 +98,10 @@ def extract_metro_colombo(_nc_fid, _date, _times):
         output_file.close()
 
 
-def extract_weather_stations(_nc_fid, _date, _times, _weather_stations):
+def extract_weather_stations(_nc_fid, _date, _times, _weather_stations, _wrf_output):
     with open(_weather_stations, 'rb') as csvfile:
         stations = csv.reader(csvfile, delimiter=' ')
-        stations_dir = wrf_output + '/RF'
+        stations_dir = _wrf_output + '/RF'
         if not os.path.exists(stations_dir):
             os.makedirs(stations_dir)
         for row in stations:
@@ -131,20 +124,70 @@ def extract_weather_stations(_nc_fid, _date, _times, _weather_stations):
             station_file.close()
 
 
-def main():
-    global wrf_home
-    global wrf_output
-    global weather_stations
-    global start_date
-    global end_date
+def extract_kelani_basin_rainfall(_nc_fid, _date, _times, _kelani_basin_file, _wrf_output):
+    lats = _nc_fid.variables['XLAT'][0, :, 0]
+    lons = _nc_fid.variables['XLONG'][0, 0, :]
 
-    wrf_home = '/mnt/disks/wrf-mod'
+    points = np.genfromtxt(_kelani_basin_file, delimiter=',')
+
+    kel_min_lon = np.min(points, 0)[1]
+    kel_min_lat = np.min(points, 0)[2]
+    kel_max_lon = np.max(points, 0)[1]
+    kel_max_lat = np.max(points, 0)[2]
+
+    lon_min = np.argmax(lons >= kel_min_lon) - 1
+    lat_min = np.argmax(lats >= kel_min_lat) - 1
+    lon_max = np.argmax(lons >= kel_max_lon)
+    lat_max = np.argmax(lats >= kel_max_lat)
+
+    prcp = _nc_fid.variables['RAINC'][:, lat_min:lat_max + 1, lon_min:lon_max + 1] + \
+           _nc_fid.variables['RAINNC'][:, lat_min:lat_max + 1, lon_min:lon_max + 1] + \
+           _nc_fid.variables['SNOWNC'][:, lat_min:lat_max + 1, lon_min:lon_max + 1] + \
+           _nc_fid.variables['GRAUPELNC'][:, lat_min:lat_max + 1, lon_min:lon_max + 1]
+
+    diff = prcp[1:73, :, :] - prcp[0:72, :, :]
+
+    kel_lats = _nc_fid.variables['XLAT'][0, lat_min:lat_max + 1, 0]
+    kel_lons = _nc_fid.variables['XLONG'][0, 0, lon_min:lon_max + 1]
+
+    def get_bins(arr):
+        sz = len(arr)
+        return (arr[1:sz - 1] + arr[0:sz - 2]) / 2
+
+    lat_bins = get_bins(kel_lats)
+    lon_bins = get_bins(kel_lons)
+
+    output_dir = _wrf_output + '/kelani-basin/created-' + _date.strftime('%Y-%m-%d')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    output_file_path = output_dir + '/RAINCELL.DAT'
+    output_file = open(output_file_path, 'w')
+
+    res = 60
+    data_hours = len(_times) - 1
+    start_ts = _date.strftime('%Y-%m-%d %H:%M:%S')
+    end_ts = (_date + dt.timedelta(hours=data_hours - 1)).strftime('%Y-%m-%d %H:%M:%S')
+    output_file.write("%d %d %s %s\n" % (res, data_hours, start_ts, end_ts))
+
+    for h in range(0, data_hours):
+        for point in points:
+            rf_x = np.digitize(point[1], lon_bins)
+            rf_y = np.digitize(point[2], lat_bins)
+            output_file.write('%d %f\n' % (point[0], diff[h, rf_y, rf_x]))
+
+    output_file.close()
+
+
+def main():
+    start_date = (dt.datetime.strptime(sys.argv[1], '%Y-%m-%d') if (len(sys.argv) > 1)
+                  else dt.datetime.today())
+    end_date = (
+        dt.datetime.strptime(sys.argv[2], '%Y-%m-%d') if (len(sys.argv) > 2) else start_date + dt.timedelta(
+            days=1))
+    wrf_home = sys.argv[3] if (len(sys.argv) > 3) else '/mnt/disks/wrf-mod'
     wrf_output = wrf_home + '/OUTPUT'
     weather_stations = wrf_home + '/wrf-scripts/src/stations.txt'
-    start_date = (dt.datetime.strptime(sys.argv[1], '%Y-%m-%d') if (len(sys.argv) > 1) else dt.datetime.today())
-    end_date = (
-        dt.datetime.strptime(sys.argv[2], '%Y-%m-%d') if (len(sys.argv) > 2) else dt.datetime.today() + dt.timedelta(
-            days=1))
 
     print "##########################"
     print "WRF dir = %s" % wrf_home
@@ -175,11 +218,16 @@ def main():
 
         print "##########################"
         print "Extract rainfall data for the metro colombo area"
-        extract_metro_colombo(nc_fid, date, times)
+        extract_metro_colombo(nc_fid, date, times, wrf_output)
 
         print "##########################"
         print "Extract weather station rainfall"
-        extract_weather_stations(nc_fid, date, times, weather_stations)
+        extract_weather_stations(nc_fid, date, times, weather_stations, wrf_output)
+
+        print "##########################"
+        print "Extract Kelani Basin rainfall"
+        kelani_basin_file = wrf_home + '/wrf-scripts/src/kelani-basin-points.txt'
+        extract_kelani_basin_rainfall(nc_fid, date, times, kelani_basin_file, wrf_output)
 
         nc_fid.close()
 
