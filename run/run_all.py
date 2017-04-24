afc69e4c5f1bedd244037d580a3572aa16aa86b0
nirandaperera <niranda.perera@gmail.com>
2017-04-24 12:06:30 +0530

    adding wrf run for a period

--- run/run_all.py
+++ run/run_all.py
@@ -5,6 +5,24 @@ import datetime as dt  # Python standard library datetime  module
 import os
 import sys
 import subprocess
+import numpy as np
+
+
+def download_gfs_data(get_data_cmd, start_date):
+    start_time = time.time()
+    process = subprocess.call('%s %s' % (get_data_cmd, start_date.strftime('%Y%m%d')), shell=True)
+    elapsed_time = time.time() - start_time
+
+    return process, elapsed_time
+
+
+def run_wrf(run_wrf_cmd, start_date, end_date):
+    start_time = time.time()
+    process = subprocess.call(
+        '%s %s %s' % (run_wrf_cmd, start_date.strftime('%Y%m%d'), end_date.strftime('%Y%m%d')), shell=True)
+    elapsed_time = time.time() - start_time
+
+    return process, elapsed_time
 
 
 def main():
@@ -16,18 +34,27 @@ def main():
     start_date = (dt.datetime.strptime(sys.argv[1], '%Y-%m-%d') if (len(sys.argv) > 1) else dt.datetime.today())
     end_date = (dt.datetime.strptime(sys.argv[2], '%Y-%m-%d') if (len(sys.argv) > 2) else
                 dt.datetime.today() + dt.timedelta(days=1))
-    print 'Downloading data for %s' % start_date
+    period = sys.argv[3] if (len(sys.argv) > 3) else 3
+
+    print 'WRF will be run from %s to %s for a period of %d days each day' % (
+        start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), period)
+
+    dates = np.arange(start_date, end_date, dt.timedelta(days=1)).astype(dt.datetime)
 
     get_data_cmd = run_home + '/get-hist-data.bash'
-    print 'Running bash file %s' % get_data_cmd
-    Process1 = subprocess.call('%s %s' % (get_data_cmd, start_date), shell=True)
-    print 'Data download completed %s' % get_data_cmd
-
     run_wrf_cmd = run_home + '/only-wrf.bash'
-    print 'Running wrf from %s to %s' % (start_date, end_date)
-    Process2 = subprocess.call(
-        '%s %s %s' % (run_wrf_cmd, start_date.strftime('%Y%m%d'), end_date.strftime('%Y%m%d')), shell=True)
-    print 'Data download completed %s' % get_data_cmd
+    for date in dates:
+        date1 = date + dt.timedelta(days=period)
+        print 'WRF scheduled from %s to %s' % (date.strftime('%Y-%m-%d'), date1.strftime('%Y-%m-%d'))
+
+        print 'Downloading data from %s. Running bash file %s' % (date.strftime('%Y%m%d'), get_data_cmd)
+        process1, elapsed_time = download_gfs_data(get_data_cmd, date)
+        print 'Data download completed for %s. Elapsed time %f' % (date.strftime('%Y%m%d'), elapsed_time)
+
+        print 'Running wrf from %s to %s' % (date.strftime('%Y%m%d'), date1.strftime('%Y%m%d'))
+        process2, elapsed_time = run_wrf_cmd(run_wrf_cmd, da)
+        print 'WRF run completed from %s to %s. Elapsed time %f' % (
+            date.strftime('%Y%m%d'), date1.strftime('%Y%m%d'), elapsed_time)
 
 
 if __name__ == "__main__":
